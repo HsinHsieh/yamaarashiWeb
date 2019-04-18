6d0a8a7476eac085dba6c1e051272f54aff25018
HsinHsieh <sam786890@gmail.com>
2019-04-18 11:34:27 +0800

    welcome!!

--- js/index.sr.js
+++ js/index.sr.js
@@ -1,49 +1,57 @@
-$(document).ready(() => {
-    ScrollReveal({ duration: 1000 });
+$(document).ready(async () => {
+    await setTimeout(() => {
+        $('.welcome').fadeOut();
 
-    bannerBG = {
-        reset: true,
-        duration: 2000,
-        easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-        scale: 1.05,
-        opacity: 1
-    }
-    ScrollReveal().reveal(".banner-bg", bannerBG);
+        ScrollReveal({ duration: 1000 });
 
-    logo_img = { reset: true };
-    logo_p = { delay: 200, reset: true };
-    ScrollReveal().reveal(".logo > img", logo_img);
-    ScrollReveal().reveal(".logo > p", logo_p);
+        bannerBG = {
+            reset: true,
+            duration: 2000,
+            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+            scale: 1.05,
+            opacity: 1
+        }
+        ScrollReveal().reveal(".banner-bg", bannerBG);
 
-    section_img = {
-        scale: .95,
-        duration: 800,
-        opacity: .6,
-        easing: 'cubic-bezier(0.19, 1, 0.22, 1)',
-        delay: 200
-    }
-    ScrollReveal().reveal(".section-img", section_img);
+        logo_img = { reset: true };
+        logo_p = { delay: 200, reset: true };
+        ScrollReveal().reveal(".logo > img", logo_img);
+        ScrollReveal().reveal(".logo > p", logo_p);
 
-    section_title = {
-        duration: 1000,
-        easing: 'cubic-bezier(0.23, 1, 0.32, 1)',
-        delay: 260
-    }
-    ScrollReveal().reveal(".section-title", section_title);
+        section_img = {
+            scale: .95,
+            duration: 800,
+            opacity: .6,
+            easing: 'cubic-bezier(0.19, 1, 0.22, 1)',
+            delay: 200
+        }
+        ScrollReveal().reveal(".section-img", section_img);
 
-    section = {
-        duration: 800,
-        easing: 'cubic-bezier(0.23, 1, 0.32, 1)',
-        delay: 320
-    }
-    ScrollReveal().reveal(".section-words, .section-button", section);
+        section_title = {
+            duration: 1000,
+            easing: 'cubic-bezier(0.23, 1, 0.32, 1)',
+            delay: 260
+        }
+        ScrollReveal().reveal(".section-title", section_title);
 
-    bannerSM = {
-        reset: true,
-        duration: 8000,
-        easing: 'cubic-bezier(1, 1, 0, 0)',
-        scale: 1.1,
-        opacity: 1
-    }
-    ScrollReveal().reveal(".banner-sm > img", bannerSM);
+        section = {
+            duration: 800,
+            easing: 'cubic-bezier(0.23, 1, 0.32, 1)',
+            delay: 320
+        }
+        ScrollReveal().reveal(".section-words, .section-button", section);
+
+        bannerSM = {
+            reset: true,
+            duration: 8000,
+            easing: 'cubic-bezier(1, 1, 0, 0)',
+            scale: 1.1,
+            opacity: 1
+        }
+        ScrollReveal().reveal(".banner-sm > img", bannerSM);
+    }, 800);
 });
+
+function HandleLogoClick() {
+    alert('你好RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR');
+};
